a311c232f86cb0257b0a911ae22268a78ce85f2a
Meheedi-Hasan <mahdyhassann@gmail.com>
2021-03-16 22:29:49 +0600

    remove product_name and changes some queries

--- routes/products.js
+++ routes/products.js
@@ -1,20 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../db');
-//var bodyParser = require('body-parser');
 
-//router.use(bodyParser.urlencoded({extended: true})); 
-
+/* get method for fetch all products.*/
 router.get('/', function(req, res, next) {
     var sql = `SELECT product_info.id,products.product_name,product_info.website_name,
     product_info.price,product_info.review,product_info.rating,
     product_info.Product_url,product_info.product_image_url 
-   FROM product_info
+    FROM product_info
     JOIN products ON products.product_id =product_info.product_id`;
 
     db.query(sql, function(err, rows, fields){
         if(err) throw err;
-        //res.json(rows);
 
         res.render('products', { title: 'Products',products: rows });
     })
@@ -25,6 +22,7 @@ router.get('/create-form', function(req, res, next){
     res.render('createform',{title: 'Create Product'});
 })
 
+  /*post method for create product*/
 router.post('/create', function(req, res, next){
     var product_name = req.body.product_name;
     var price = req.body.price;
@@ -34,7 +32,7 @@ router.post('/create', function(req, res, next){
     var Product_url = req.body.Product_url;
     var product_image_url = req.body.product_image_url;
 
-    var sql =  `INSERT INTO products (product_name, website_name, price, review, rating, Product_url,product_image_url ) VALUES ("${product_name}", "${sku}", "${price}", 1, NOW())`;
+    var sql =  `INSERT INTO products (product_name, website_name, price, review, rating, Product_url,product_image_url ) VALUES ("${product_name}", "${website_name}", "${price}","${review}","${rating}","${Product_url}","${product_image_url}")`;
     db.query(sql, function(err, result){
         if(err) throw err;
         res.redirect('/products');
@@ -44,7 +42,7 @@ router.post('/create', function(req, res, next){
 
 router.get('/edit-form/:id', function(req, res, next){
     var id = req.params.id;
-    var sql = `SELECT product_info.id,products.product_name,product_info.website_name,
+    var sql = `SELECT product_info.id,product_info.website_name,
     product_info.price,product_info.review,product_info.rating,
     product_info.Product_url,product_info.product_image_url 
    FROM product_info
@@ -56,7 +54,7 @@ router.get('/edit-form/:id', function(req, res, next){
 })
 
 router.post('/edit/:id', function(req, res, next){
-    var product_name = req.body.product_name;
+    //var product_name = req.body.product_name;
     var price = req.body.price;
     var website_name = req.body.website_name;
     var review = req.body.review;
@@ -66,7 +64,7 @@ router.post('/edit/:id', function(req, res, next){
 
     var id = req.params.id;
 
-    var sql =  `UPDATE products SET product_name="${product_name}", sku="${sku}", price="${price}" WHERE id=${id}`;
+    var sql =  `UPDATE product_info SET  price="${price}", website_name="${website_name}",review="${review}", rating="${rating}" , Product_url="${Product_url}" , product_image_url="${product_image_url}" WHERE id=${id}`;
     db.query(sql, function(err, result){
         if(err) throw err;
         res.redirect('/products');
@@ -76,8 +74,7 @@ router.post('/edit/:id', function(req, res, next){
 
 router.get('/delete/:id', function(req,res,next){
     var id = req.params.id;
-    var sql=`DELETE FROM product_info
-    JOIN products ON products.product_id =product_info.product_id WHERE id=${id}`;
+    var sql=`DELETE FROM product_info WHERE id=${id}`;
     db.query(sql, function(err, result){
         if(err) throw err;
         res.redirect('/products');
