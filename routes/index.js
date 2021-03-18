b0a95bd9dccfd30fb71ca04e86e0480ba5116464
Meheedi-Hasan <mahdyhassann@gmail.com>
2021-03-19 00:39:56 +0600

    create login page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
+var db = require('../db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Login Page' });
+});
+
+router.post('/login', function(req, res, next){
+  var username = req.body.username;
+  var password = req.body.password;
+
+    if (username=='mehedi' && password == 'mypassword') {
+       res.redirect('/products');
+    } else { 
+      console.log("Login failed");
+    }
+
 });
 
 
