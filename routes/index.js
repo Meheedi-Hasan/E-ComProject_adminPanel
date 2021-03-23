e6add9587514990acbf76cb7151bb4b50ff4226e
Meheedi-Hasan <mahdyhassann@gmail.com>
2021-03-24 01:23:12 +0600

    validated username, password with database

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,40 @@ var router = express.Router();
 var db = require('../db');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Login Page' });
-});
+ router.get('/', function(req, res, next) {
+   res.render('index', { title: 'Login Page' });
+ });
 
-router.post('/login', function(req, res, next){
+router.post('/log', function(req, res, next){
   var username = req.body.username;
   var password = req.body.password;
 
-    if (username=='mehedi' && password == 'mypassword') {
-       res.redirect('/products');
-    } else { 
-      console.log("Login failed");
-    }
+  if (username && password) {
+		db.query('SELECT * FROM login WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				req.session.loggedin = true;
+				req.session.username = username;
+				res.redirect('/products');
+			} else {
+				res.send('Incorrect Username and/or Password!');
+			}			
+			res.end();
+		});
+	} else {
+		res.send('Please enter Username and Password!');
+		res.end();
+	}
 
-});
+  // console.log(`${username}`);
+  // console.log(`${password}`);
+  // if((`${username}` == 'mehedi') && (`${password}`==='123')){
+  //   res.redirect('/products');
+  // }else{
+  //   console.log("Login failed");
+  // }
+
+  
+})
 
 
 module.exports = router;
