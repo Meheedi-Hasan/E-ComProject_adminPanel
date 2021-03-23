1f7354a66446be862a623c5b1bfaecd09334ed4c
Meheedi-Hasan <mahdyhassann@gmail.com>
2021-03-24 01:26:40 +0600

    added some codes to do better

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,25 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('respond with a resource');
 });
 
 module.exports = router;
+
+/*
+router.post('/regi', function(req, res, next){
+  var username = req.body.username;
+  var email = req.body.email;
+  var password = req.body.password;
+
+    console.log(`${username}`);
+
+//   var sql = `INSERT INTO login (username, email, password) VALUES ("${username}", "${email}", "${password}")`;  
+  
+//   db.query(sql, function(err, result){
+//       if(err) throw err;
+//       res.redirect('/index');
+
+//   })
+// })
+*/
