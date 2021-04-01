2f9455dbf5f0706877c6c2340f70550d2816f71f
Meheedi-Hasan <mahdyhassann@gmail.com>
2021-04-02 00:58:10 +0600

    secured login

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../db');
+var md5 = require('md5');
 
 /* GET home page. */
  router.get('/', function(req, res, next) {
@@ -8,34 +9,29 @@ var db = require('../db');
  });
 
 router.post('/log', function(req, res, next){
-  var username = req.body.username;
-  var password = req.body.password;
+  var email = req.body.email;
+  var password = md5(req.body.password);
+  //var password = md5(n_password);
 
-  if (username && password) {
-		db.query('SELECT * FROM login WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+//   console.log('Normal password : ', n_password)
+//   console.log('Hashed password : ', password)
+
+  if (email && password) {
+		db.query('SELECT * FROM login WHERE email = ? AND password = ?', [email, password], function(error, results, fields) {
 			if (results.length > 0) {
 				req.session.loggedin = true;
-				req.session.username = username;
+				req.session.email = email;
 				res.redirect('/products');
 			} else {
-				res.send('Incorrect Username and/or Password!');
+				res.send('Incorrect email and/or Password!');
 			}			
 			res.end();
 		});
 	} else {
-		res.send('Please enter Username and Password!');
+		res.send('Please enter email and Password!');
 		res.end();
 	}
 
-  // console.log(`${username}`);
-  // console.log(`${password}`);
-  // if((`${username}` == 'mehedi') && (`${password}`==='123')){
-  //   res.redirect('/products');
-  // }else{
-  //   console.log("Login failed");
-  // }
-
-  
 })
 
 
